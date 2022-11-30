d761e8438a70084906dcb05731b4abeebb3f4275
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-11-30 17:27:57 +0530

    smartly adding hover to cell in barchart.

--- src/baseComponents/Chart.jsx
+++ src/baseComponents/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   BarChart,
   Bar,
@@ -13,50 +13,49 @@ import { useState } from "react";
 
 function Chart() {
   const [focusBar, setFocusBar] = useState(null);
-  const [mouseLeave, setMouseLeave] = useState(true);
-  const [dataG, setDataG] = useState(JSON.parse(JSON.stringify(data)));
 
-  const date = new Date();
-  let day = date.getDay();
-  if (day === 0) {
-    day = 7;
-  }
-
-  useEffect(() => {
-    const newData = dataG.map((data, index) => {
-      if (index === day) {
-        return { ...data, color: "hsl(186, 34%, 60%)" };
+  function setHoverColor(focus, index) {
+    const date = new Date();
+    let day = date.getDay();
+    if (day === 0) {
+      day = 7;
+    }
+    const currentDayColor = "hsl(186, 34%, 60%)";
+    const currentDayHover = "hsl(186, 34%, 72%)";
+    const baseColor = "hsl(10, 79%, 65%)";
+    const baseHover = "hsl(10, 79%, 80%)";
+    if (focus === index) {
+      if (day === index + 1) {
+        return currentDayHover;
       } else {
-        return { ...data, color: "hsl(10, 79%, 65%)" };
+        return baseHover;
       }
-    });
-    setDataG(newData);
-  }, []);
+    } else {
+      if (day === index + 1) {
+        return currentDayColor;
+      } else {
+        return baseColor;
+      }
+    }
+  }
 
   return (
     <ResponsiveContainer width="100%" aspect={2}>
       <BarChart
-        data={dataG}
+        data={data}
         width={462}
         height={218}
         onMouseMove={(state) => {
           if (state.isTooltipActive) {
             setFocusBar(state.activeTooltipIndex);
-            setMouseLeave(false);
           } else {
             setFocusBar(null);
-            setMouseLeave(true);
           }
         }}
       >
-        <Bar dataKey="amount" fill={dataG.color} radius={[5, 5, 5, 5]}>
-          {dataG.map((entry, index) => (
-            <Cell
-              key={index}
-              fill={
-                focusBar === index || mouseLeave ? entry.color : entry.color
-              }
-            />
+        <Bar dataKey="amount" fill="hsl(10, 79%, 65%)" radius={[5, 5, 5, 5]}>
+          {data.map((entry, index) => (
+            <Cell key={index} fill={setHoverColor(focusBar, index)} />
           ))}
         </Bar>
         <XAxis
@@ -74,9 +73,5 @@ function Chart() {
     </ResponsiveContainer>
   );
 }
-
-// /width={462} height={218}
-//<div className="w-[21.25rem] md:w-[31.25rem] lg:w-[31.25rem]">
-//</div>
 
 export default Chart;
